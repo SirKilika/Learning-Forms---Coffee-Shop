8391db62dc4d6a00138bf34a09eaecdf4959defe
SirKilika <fabiomacielsilva87@gmail.com>
2023-04-29 13:57:03 +0100

    Learning Forms

--- coffeeshop/coffeeshop/views.py
+++
@@ -1,7 +0,0 @@
-from django.template import loader
-from django.http import HttpResponse
-
-def Home(request):
-    template = loader.get_template("home.html")
-    context = {}
-    return HttpResponse(template.render(context,request))
